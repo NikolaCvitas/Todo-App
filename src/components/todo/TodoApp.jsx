8aeaac583f615785156446fb7f9df91513e8585d
Nikola Cvitas <ncvitas@realnetworks.com>
2020-12-09 15:31:56 +0100

    event handler

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -21,8 +21,8 @@ class LoginComponent extends Component{
             password : ''
         }
 
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+       // this.handleUsernameChange = this.handleUsernameChange.bind(this);
+      //  this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
