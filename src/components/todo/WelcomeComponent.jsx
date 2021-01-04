82bff5a5a9e38d05648c60e642112ac6d11b4674
Nikola Cvitas <ncvitas@realnetworks.com>
2021-01-04 13:52:27 +0100

    set port to 4200

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Link} from 'react-router-dom';
 
 class WelcomeComponent extends Component{
+
+    constructor(props){
+        super(props)
+        this.retreiveWelcomeMessage = this.retreiveWelcomeMessage.bind(this)
+    }
     render(){
         return(
 
@@ -9,12 +14,25 @@ class WelcomeComponent extends Component{
 
             <h1>Welcome!</h1>
             <div className="container">
-            Welcome {this.props.match.params.name} . You can manage your todos <Link to="/todos">here</Link>.
+            Welcome {this.props.match.params.name} .
+             You can manage your todos <Link to="/todos">here</Link>.
+            </div>
+
+            <div className="container">
+            Click here to get customized welcome message.
+            <button onClick={this.retreiveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
             </div>
             </>
 
         )
     }
+
+    retreiveWelcomeMessage() {
+
+        console.log("retreive clicked")
+    }
 }
+
+
 
 export default WelcomeComponent; 
