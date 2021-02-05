3d62ed832b9e17793a8e1720530bbb7cddc17690
Nikola Cvitas <ncvitas@realnetworks.com>
2021-02-05 14:32:59 +0100

    Enhancing React Welcome Data Service to use Basic Auth

--- src/api/todo/TestService.js
+++ src/api/todo/TestService.js
@@ -20,9 +20,24 @@ class TestService {
 
     executeTestPathVariableService(name) {
 
-        // console.log('executed service')
+         console.log('executed service')
 
-       return axios.get(`http://localhost:8080//test/var/${name}`);
+        let username = 'nikola'
+        let password = '1234'
+
+        let basicAuthHeaderString = 'Basic ' + window.btoa(`${username}:${password}`);
+
+       // let headers = new Headers();
+
+      //  headers.append('Content-Type', 'application/json');
+      //  headers.append('Accept', 'application/json');
+      //  headers.append('Authorization', 'Basic ' + window.btoa(username + ":" +  password));
+       // headers.append('Access-Control-Allow-Origin','*');
+
+
+       return axios.get(`http://localhost:8080/test/var/${name}`,
+       {headers :{ Authorization: basicAuthHeaderString }
+    });
        
     }
  
